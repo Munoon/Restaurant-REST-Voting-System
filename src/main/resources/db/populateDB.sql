f8fca009ec40e94763e2228b41f87c497ae721de
Munoon <munoongg@gmail.com>
2019-06-29 15:31:13 +0300

    Initialized initDB and populateDB

---
+++ src/main/resources/db/populateDB.sql
@@ -0,0 +1,30 @@
+DELETE FROM user_votes;
+DELETE FROM menu;
+DELETE FROM restaurants;
+DELETE FROM user_roles;
+DELETE FROM users;
+ALTER SEQUENCE GLOBAL_SEQ RESTART WITH 100;
+
+INSERT INTO users (name, email, password) VALUES
+    ('Nikita', 'munoon@yandex.ru', 'easyPass'),
+    ('Vasya', 'vasya@gmail.com', 'VasyaTheBest'),
+    ('Petr', 'petr@gmail.com', 'PertHasStrongPass123');
+
+INSERT INTO user_roles (user_id, role) VALUES
+    (100, 'ROLE_ADMIN'),
+    (101, 'ROLE_USER'),
+    (102, 'ROLE_USER');
+
+INSERT INTO restaurants (name) VALUES
+    'McDonalds',
+    'KFC';
+
+INSERT INTO menu (restaurant_id, name, price) VALUES
+    (103, 'Burger', 50),
+    (103, 'French Fries', 20),
+    (104, 'Burger', 30),
+    (104, 'Chicken', 35);
+
+INSERT INTO user_votes (user_id, restaurant_id) VALUES
+    (100, 104),
+    (101, 103);

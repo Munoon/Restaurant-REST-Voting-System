8ccf740f0d4598232a8c29e37d5decfe2ce24cdc
Munoon <munoongg@gmail.com>
2019-08-23 19:33:47 +0300

    Add Authentication Principal

--- src/main/java/com/train4game/munoon/web/controllers/VoteRestController.java
+++ src/main/java/com/train4game/munoon/web/controllers/VoteRestController.java
@@ -1,27 +1,26 @@
 package com.train4game.munoon.web.controllers;
 
+import com.train4game.munoon.AuthorizedUser;
 import com.train4game.munoon.model.Restaurant;
 import com.train4game.munoon.model.Vote;
 import com.train4game.munoon.service.RestaurantService;
 import com.train4game.munoon.service.VoteService;
 import com.train4game.munoon.to.VoteTo;
-import com.train4game.munoon.utils.ParserUtil;
 import com.train4game.munoon.web.SecurityUtil;
 import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeMap;
-import org.modelmapper.TypeToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
-import java.lang.reflect.Type;
 import java.net.URI;
 import java.util.List;
 
@@ -53,34 +52,30 @@ public class VoteRestController {
     }
 
     @GetMapping
-    public List<VoteTo> getAll() {
-        int userId = SecurityUtil.authUserId();
-        log.info("Get all votes of user {}", userId);
-        return modelMapper.map(service.getAll(userId), VOTE_LIST_MAPPER);
+    public List<VoteTo> getAll(@AuthenticationPrincipal AuthorizedUser user) {
+        log.info("Get all votes of user {}", user.getId());
+        return modelMapper.map(service.getAll(user.getId()), VOTE_LIST_MAPPER);
     }
 
     @GetMapping("/{id}")
-    public VoteTo get(@PathVariable int id) {
-        int userId = SecurityUtil.authUserId();
-        log.info("Get vote {} with user {}", id, userId);
-        return toVoteTo.map(service.get(id, userId));
+    public VoteTo get(@PathVariable int id, @AuthenticationPrincipal AuthorizedUser user) {
+        log.info("Get vote {} with user {}", id, user.getId());
+        return toVoteTo.map(service.get(id, user.getId()));
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable int id) {
-        int userId = SecurityUtil.authUserId();
-        log.info("Delete vote {} with user {}", id, userId);
-        service.delete(id, userId);
+    public void delete(@PathVariable int id, @AuthenticationPrincipal AuthorizedUser user) {
+        log.info("Delete vote {} with user {}", id, user.getId());
+        service.delete(id, user.getId());
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void update(@Valid @RequestBody VoteTo voteTo, @PathVariable int id) {
-        int userId = SecurityUtil.authUserId();
+    public void update(@Valid @RequestBody VoteTo voteTo, @PathVariable int id, @AuthenticationPrincipal AuthorizedUser user) {
         assureIdConsistent(voteTo, id);
-        log.info("Update {} with id {} from user {}", voteTo, id, userId);
-        service.update(toVote.map(voteTo), userId);
+        log.info("Update {} with id {} from user {}", voteTo, id, user.getUser());
+        service.update(toVote.map(voteTo), user.getId());
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -93,9 +88,8 @@ public class VoteRestController {
     }
 
     private VoteTo create(VoteTo voteTo) {
-        int userId = SecurityUtil.authUserId();
         log.info("Create {}", voteTo);
         checkNew(voteTo);
-        return toVoteTo.map(service.create(toVote.map(voteTo), userId));
+        return toVoteTo.map(service.create(toVote.map(voteTo), SecurityUtil.authUserId()));
     }
 }
